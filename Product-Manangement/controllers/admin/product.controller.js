1a46a997e9d6fda93782627a2aef30bbdd51f620
thanhan06 <nguyenvanvu060104@gmail.com>
2024-11-03 18:06:04 +0700

    Filter

--- Product-Manangement/controllers/admin/product.controller.js
+++ Product-Manangement/controllers/admin/product.controller.js
@@ -2,6 +2,38 @@ const Product= require("../../models/product.model")
 
 // [GET] /admin/products
 module.exports.index = async (req,res)=>{
+// Danh sách button bên file page/admin/product/index.pug
+    let filterStatus = [
+        {
+            name : "Tất cả",
+            status:"",
+            class:""
+
+        },
+        {
+            name : "Hoạt động",
+            status:"active",
+            class:""
+
+            
+        },
+        {
+            name : "Dừng hoạt động",
+            status:"inactive",
+            class:""
+
+
+        }
+    ]
+    if(req.query.status)
+    {
+        const index = filterStatus.findIndex(item =>item.status == req.query.status )
+        filterStatus[index].class ="active";
+    }else{
+        const index = filterStatus.findIndex(item =>item.status =="" )
+        filterStatus[index].class ="active";
+    }
+
 
     // console.log(req.query.status);
     let find={
@@ -18,8 +50,8 @@ module.exports.index = async (req,res)=>{
     console.log(products);
         res.render("admin/pages/products/index",{
             pageTitle :"Trang sản phẩm",
-            products: products
-
+            products: products,
+            filterStatus : filterStatus
         }
         );
     
--- Product-Manangement/public/admin/js/script.js
+++ Product-Manangement/public/admin/js/script.js
@@ -2,10 +2,21 @@
 const buttonStatus = document.querySelectorAll('[button-status]');
 
 if(buttonStatus.length>0){
+
+    let url = new URL(window.location.href);
+    console.log(url);
+
     buttonStatus.forEach(button=>{
         button.addEventListener("click",()=>{
             const status = button.getAttribute("button-status");
-            console.log(status);
+            
+            if(status){
+                url.searchParams.set("status",status);  //phần sau dấu hỏi nta gọi là params
+            } else {
+                url.searchParams.delete("status");
+            }
+
+            window.location.href = url.href;
         });
 
     });
