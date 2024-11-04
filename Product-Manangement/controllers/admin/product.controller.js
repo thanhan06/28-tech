6a02f2c30f773c13dede2c816e75511cd6afbd62
thanhan06 <nguyenvanvu060104@gmail.com>
2024-11-04 16:49:58 +0700

    tối ưu bộ lọc

--- Product-Manangement/controllers/admin/product.controller.js
+++ Product-Manangement/controllers/admin/product.controller.js
@@ -1,40 +1,9 @@
 const Product= require("../../models/product.model")
-
+const filterStatusHelper = require("../../helpers/filterStatus")
 // [GET] /admin/products
 module.exports.index = async (req,res)=>{
-// Danh sách button bên file page/admin/product/index.pug
-    let filterStatus = [
-        {
-            name : "Tất cả",
-            status:"",
-            class:""
-
-        },
-        {
-            name : "Hoạt động",
-            status:"active",
-            class:""
-
-            
-        },
-        {
-            name : "Dừng hoạt động",
-            status:"inactive",
-            class:""
-
-
-        }
-    ]
-    if(req.query.status)
-    {
-        const index = filterStatus.findIndex(item =>item.status == req.query.status )
-        filterStatus[index].class ="active";
-    }else{
-        const index = filterStatus.findIndex(item =>item.status =="" )
-        filterStatus[index].class ="active";
-    }
-
-
+    // Đoạn bộ lọc
+    const filterStatus = filterStatusHelper(req.query);
     // console.log(req.query.status);
     let find={
         deleted: false
